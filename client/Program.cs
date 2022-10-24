21581cc20ae0ae4117605507b18a3bec9cb37fba
Ertugrul Karaduman <0997475@hr.nl>
2022-10-25 00:35:52 +0200

    Bijna klaar

--- client/Program.cs
+++ client/Program.cs
@@ -17,6 +17,8 @@ namespace Client
             byte[] buffer = new byte[(int)Params.BUFFER_SIZE];
             byte[] msg = new byte[1024];
             byte[] revReqMsg = new byte[1024];
+            byte[] revackMsg = new byte[1024];
+            byte[] revcloMsg = new byte[1024];
             // TODO: Initialise the socket/s as needed from the description of the assignment
             Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
             IPAddress ip = IPAddress.Parse("127.0.0.1");
@@ -44,6 +46,18 @@ namespace Client
             c.ConID = chosenId;
             c.From = student_1;
             c.To = "Server";
+            c.Sequence = 0;
+
+            ack.From = student_1;
+            ack.To = "Server";
+            ack.Sequence = 0;
+            ack.ConID = chosenId;
+            ack.Type = Messages.ACK;
+
+            cls.ConID = chosenId;
+            cls.From = student_1;
+            cls.To = "Server";
+            cls.Type = Messages.CLOSE_CONFIRM;
             
             string req = "Type of message:" + r.Type + " & " + " File name:" + r.FileName + "& " + " Id:" + c.ConID;
 
@@ -55,8 +69,8 @@ namespace Client
                 sock.SendTo(msg, endpoint);
                 
                 // TODO: Receive and verify a HelloMSG 
-                int recv = sock.ReceiveFrom(msg, SocketFlags.None, ref remoteEP);
-                Console.WriteLine("Messaged received from: {0} and the message is: {1}",h.From, Encoding.ASCII.GetString(msg,0,recv));
+                //int recv = sock.ReceiveFrom(msg, SocketFlags.None, ref remoteEP);
+                //Console.WriteLine("Messaged received from: {0} and the message is: {1}",h.From, Encoding.ASCII.GetString(msg,0,recv));
 
                 // TODO: Send the RequestMSG message requesting to download a file name
                 byte[] reqMSG = Encoding.ASCII.GetBytes(req);
@@ -73,6 +87,36 @@ namespace Client
 
                 // TODO: Check if there are more DataMSG messages to be received 
                 // receive the message and verify if there are no errors
+                string fullText = "";
+                while(true)
+                {
+                    int x = sock.ReceiveFrom(revackMsg, SocketFlags.None, ref remoteEP);
+                    Console.WriteLine("Message received from {0} and the message is: {1}", r.From, Encoding.ASCII.GetString(revackMsg, 0, x));
+                    fullText += Encoding.ASCII.GetString(revackMsg, 0, x);
+                    ack.Sequence++;
+                    if(ErrorHandler.VerifyAck(ack, c) == ErrorType.NOERROR)
+                    {
+                        //if message correct, send correct back
+                        sock.SendTo(Encoding.ASCII.GetBytes(Messages.ACK.ToString()), remoteEP);
+                    } else {
+                        sock.SendTo(Encoding.ASCII.GetBytes(ack.Sequence.ToString()), remoteEP);
+                    }
+                    if((int)Params.WINDOW_SIZE == ack.Sequence)
+                    {
+                        break;
+                    }
+                }
+                //Console.WriteLine(fullText);
+                
+                int xd = sock.ReceiveFrom(revcloMsg, SocketFlags.None, ref remoteEP);
+                Console.WriteLine("Message received from {0} and the message is: {1}", r.From, Encoding.ASCII.GetString(revcloMsg, 0, xd));
+                
+                if(ErrorHandler.VerifyClose(cls, c) == ErrorType.NOERROR)
+                {
+                        sock.SendTo(Encoding.ASCII.GetBytes(Messages.CLOSE_CONFIRM.ToString()+"|"+cls.ConID), remoteEP);
+                }
+
+                
 
                 // TODO: Send back AckMSG for each received DataMSG 
 
--- file_download_server/File Handler/Communicate.cs
+++ file_download_server/File Handler/Communicate.cs
@@ -23,6 +23,8 @@ namespace UDP_FTP.File_Handler
         private byte[] buffer;
         byte[] msg;
         byte[] reqMSG;
+        byte[] revackMsg;
+        byte[] revclsMsg;
         private string file;
         ConSettings C;
 
@@ -36,6 +38,8 @@ namespace UDP_FTP.File_Handler
             msg = new byte[1024];
 
             reqMSG = new byte[1024];
+            revackMsg = new byte[1024];
+            revclsMsg = new byte[1024];
 
             Random id = new Random();
             SessionID = id.Next(1, 40);
@@ -69,6 +73,11 @@ namespace UDP_FTP.File_Handler
 
             c.To = Client;
             c.From = Server;
+            c.Sequence = 0;
+
+            cls.From = Server;
+            cls.To = Client;
+            cls.Type = Messages.CLOSE_CONFIRM;
 
 
             // TODO: Start the communication by receiving a HelloMSG message
@@ -115,12 +124,106 @@ namespace UDP_FTP.File_Handler
 
             // TODO:  Start sending file data by setting first the socket ReceiveTimeout value
             socket.ReceiveTimeout = 5000;
-            string text = File.ReadAllText("../../../" + req.FileName);
-            Console.WriteLine(text.Length);
-            Console.WriteLine(text);
-            // 50 char in 12 pakketen
+            string text = "";
+            if(OperatingSystem.IsWindows()){
+                text = File.ReadAllText(req.FileName);
+            }
+            if(OperatingSystem.IsMacOS()){
+                text = File.ReadAllText("../../../" + req.FileName);
+            }
+            
+            byte[] fileBytes = File.ReadAllBytes(req.FileName); //Convert tekst file into bytes
+            byte[][] chunk = new byte[(int)Params.WINDOW_SIZE][]; //prepare byte array with size of windows size
 
+            //variables configuration
+            int maxFileSize = 0;
+            int maxChuckSize = 0;
+            int fileIndex = 0;
+            int checkWindowSize = 0;
 
+            
+            while(true)
+            {
+                for (int i = 0; i < chunk.Length; i++)
+                {
+                    chunk[i] = new byte[fileBytes.Length / (int)Params.SEGMENT_SIZE]; //Calculate bytes one chunk send and make array
+                    for (int j = 0; j < chunk[i].Length; j++) //Loops calculated chunk untill it is full or ends
+                    {
+                        chunk[i][j] = fileBytes[fileIndex]; //Fill chunk of window size with value fileIndex | chunk = [windowsize][calculated byte from the message]
+                        fileIndex++;
+                        if(fileIndex == fileBytes.Length) //If index equals file.length. end loop
+                        {
+                            break;
+                        }
+                    }
+                    socket.SendTo(chunk[i], remoteEP);
+                    
+
+                    checkWindowSize++;
+                    if(checkWindowSize == (int)Params.WINDOW_SIZE)
+                    {
+                        socket.SendTimeout = 1000;
+                        int max = 1;
+                        while(true)
+                        {
+                            int x = socket.ReceiveFrom(revackMsg, SocketFlags.None, ref remoteEP);
+                            Console.WriteLine("Message received from {0} and the message is: {1}", req.From, Encoding.ASCII.GetString(revackMsg, 0, x));
+                            Console.WriteLine(max);
+                            if(max == 5)
+                            {
+                                break;
+                            }
+                            max++;
+                        }
+                        break;
+                    }
+                    //Console.WriteLine(Encoding.ASCII.GetString(chunk[i]));
+                    if(fileIndex == fileBytes.Length)
+                    {
+                        socket.SendTo(Encoding.ASCII.GetBytes(Messages.CLOSE_REQUEST.ToString()), remoteEP);
+                        break;
+                    }
+                    
+                }
+                
+                if(fileIndex == fileBytes.Length)
+                {
+                    break;
+                }
+
+                
+                
+                // if (checkWindowSize == (int)Params.WINDOW_SIZE)
+                // {
+                //     checkWindowSize = 0;
+                //     for (int i = 0; i < (int)Params.WINDOW_SIZE; i++)
+                //     {
+                //         //send packages
+                //     }
+                // }
+
+                // if (maxFileSize == fileBytes.Length)
+                // {
+                //     //Max size reached of array
+                // }
+            }
+            
+            int xd = socket.ReceiveFrom(revclsMsg, SocketFlags.None, ref remoteEP);
+            Console.WriteLine("Message received from {0} and the message is: {1}", req.From, Encoding.ASCII.GetString(revclsMsg, 0, xd));
+            cls.ConID = Int32.Parse(Encoding.ASCII.GetString(revclsMsg, 0, xd).Split("|")[1]);
+            c.ConID = Int32.Parse(Encoding.ASCII.GetString(revclsMsg, 0, xd).Split("|")[1]);
+            if(ErrorHandler.VerifyClose(cls, c) == ErrorType.NOERROR)
+            {
+                socket.Close();
+            }
+            
+            // Console.WriteLine("--------------------------------");
+            // foreach(var x in chunk)
+            // {
+            //     Console.WriteLine(Encoding.ASCII.GetString(x));
+            // }
+            // Console.WriteLine("--------------------------------");
+            
 
             // TODO: Open and read the text-file first
             // Make sure to locate a path on windows and macos platforms
