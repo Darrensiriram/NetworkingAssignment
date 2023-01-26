4633dcc32161b55137e9f5644a2839502eb34136
Darren Siriram <ds.siriram@gmail.com>
2023-01-26 21:10:59 +0100

    check why request message is not working..

--- client/Program.cs
+++ client/Program.cs
@@ -18,11 +18,12 @@ namespace Client
             string student_2 = "Ertugrul Karaduman 0997475";
             
             List<int> LostAck = new List<int> { 2, 5, 7 };
+            byte[] revmsg = new byte[1024];
+            byte[] buffer = new byte[2048];
+            Socket sock;
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 32000);
+            EndPoint remoteEp = new IPEndPoint(IPAddress.Any, 32000);
             
-            int port = 32000;
-            IPAddress serverIP = IPAddress.Parse("127.0.0.1");
-            IPEndPoint serverEndPoint = new IPEndPoint(serverIP, port);
-            UdpClient client = new UdpClient(); 
 
             HelloMSG h = new HelloMSG();
             RequestMSG r = new RequestMSG();
@@ -35,40 +36,72 @@ namespace Client
             int windowSize = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter the segment size:");
             int segmentSize = int.Parse(Console.ReadLine());
-
-            h.ConID = -1;
+        
+            h.ConID = 123;
             h.From = "client1";
             h.Type = Enums.Messages.HELLO;
             h.To = "MyServer";
             byte[] helloBytes = JsonSerializer.SerializeToUtf8Bytes(h);
             
+            
             try
             {
-                client.Send(helloBytes, helloBytes.Length, serverEndPoint);
-                // TODO: Receive and verify a HelloMSG 
-                byte[] receivedBytes = client.Receive(ref serverEndPoint);
-                HelloMSG receivedHello = JsonSerializer.Deserialize<HelloMSG>(receivedBytes);
-                if (ErrorHandler.VerifyGreeting(receivedHello, c) == ErrorType.NOERROR)
-                {
-                    Console.WriteLine("Error: Not a Hello message");
-                    return;
-                }
-                // TODO: Send the RequestMSG message requesting to download a file name
-                // In this message you need to send window and sequence sizes
+                sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                sock.SendTo(helloBytes, helloBytes.Length, SocketFlags.None, endPoint);
 
+                int b = sock.ReceiveFrom(buffer, ref remoteEp);
+                var utf8Reader = new Utf8JsonReader(buffer);
+                HelloMSG H = JsonSerializer.Deserialize<HelloMSG>(ref utf8Reader);
+                
                 r.From = h.From;
+                r.To = h.To;
                 r.ConID = h.ConID;
                 r.Type = Enums.Messages.REQUEST;
-                r.To = h.To;
                 r.FileName = "test.txt";
                 r.WindowsSize = windowSize;
                 r.SegmentSize = segmentSize;
+
+                byte[] requestByte = JsonSerializer.SerializeToUtf8Bytes(r);
+                sock.SendTo(requestByte, requestByte.Length, SocketFlags.None, endPoint);
                 
-                byte[] requestBytes = JsonSerializer.SerializeToUtf8Bytes(r);
-                client.Send(requestBytes, requestBytes.Length, serverEndPoint);
+                int reqmsg = sock.ReceiveFrom(buffer, ref remoteEp);
+                RequestMSG R = JsonSerializer.Deserialize<RequestMSG>(ref utf8Reader);
+                
+                // TODO:  Check why R is not giving any reponse, also use error handler VerifyRequest to check response.
+                
+                int dataMsg = sock.ReceiveFrom(buffer, ref remoteEp);
+                DataMSG DMSG = JsonSerializer.Deserialize<DataMSG>(ref utf8Reader);
+
+                int nextSeqNum = 0;
+                while(nextSeqNum < windowSize)
+                {
+                    int receivedDataBytes = sock.ReceiveFrom(buffer, ref remoteEp);
+                    DataMSG receivedData = JsonSerializer.Deserialize<DataMSG>(receivedDataBytes);
+                    
+                    if (receivedData.Sequence == nextSeqNum)
+                    {
+                        nextSeqNum++;
+                        
+                        if (LostAck.Contains(receivedData.Sequence))
+                        {
+                            LostAck.Remove(receivedData.Sequence);
+                            continue;
+                        }
+                        
+                        ack.Type = Messages.ACK;
+                        ack.Sequence = receivedData.Sequence;
+                        byte[] ackBytes = JsonSerializer.SerializeToUtf8Bytes(ack);
+                        sock.SendTo(ackBytes, ackBytes.Length, SocketFlags.None, endPoint);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Unexpected sequence number");
+                    }
+                }
+
 
             }
-            catch
+            catch (SocketException e)
             {
                 Console.WriteLine("Error: An exception has occurred.");
             }
