72f7f8ef682146d25a96994c3aec28109081476b
Darren Siriram <ds.siriram@gmail.com>
2023-02-04 15:24:50 +0100

    server side, waiting to implement window slide -n functionality

--- file_download_server/File Handler/Communicate.cs
+++ file_download_server/File Handler/Communicate.cs
@@ -22,21 +22,23 @@ namespace UDP_FTP.File_Handler
         private ErrorType Status;
         private byte[] buffer;
         byte[] msg;
+        byte[] dbuffer;
         private string file;
         ConSettings C;
 
 
         public Communicate()
         {
-            remoteEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 32000);
-            remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 32000);
+            remoteEndpoint = new IPEndPoint(IPAddress.Any, 32000);
+            remoteEP = new IPEndPoint(IPAddress.Any, 32000);
             socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
             socket.Bind(remoteEndpoint);
             // socket.Listen(10);
             
             msg = new byte[2048];
             buffer = new byte[2048];
-            
+            dbuffer = new byte[2048];
+
             Random id = new Random();
             SessionID = id.Next(1, 1000);
             
@@ -55,11 +57,87 @@ namespace UDP_FTP.File_Handler
 
             int HelloReceived = socket.ReceiveFrom(buffer, ref remoteEP);
             var utf8Reader = new Utf8JsonReader(buffer);
-            HelloMSG GREETBACK = JsonSerializer.Deserialize<HelloMSG>(ref utf8Reader);
+            HelloMSG receivedHelloMessage = JsonSerializer.Deserialize<HelloMSG>(ref utf8Reader);
+            Client = receivedHelloMessage.From;
+            
+            C.To = receivedHelloMessage.To;
+            C.Type = receivedHelloMessage.Type;
 
-           Console.WriteLine(GREETBACK.Type);
-           return ErrorType.NOERROR;
+            if (ErrorHandler.VerifyGreeting(receivedHelloMessage, C) != ErrorType.NOERROR)
+            {
+                Console.WriteLine("Error: Wrong message type withing the greeting phase");
+                return ErrorType.BADREQUEST;
+            }
+            try
+            {
+                GreetBack.From = Server;
+                GreetBack.To = Client;
+                GreetBack.ConID = SessionID;
+                GreetBack.Type = Messages.HELLO_REPLY;
+                byte[] helloReplyBytes = JsonSerializer.SerializeToUtf8Bytes(GreetBack);
+                socket.SendTo(helloReplyBytes, helloReplyBytes.Length, SocketFlags.None, remoteEP);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return ErrorType.BADREQUEST;
+            }
+            
+            
+            int requestReceived = socket.ReceiveFrom(msg, ref remoteEP);
+            var utf8Reader2 = new Utf8JsonReader(msg);
+            RequestMSG receivedRequestMessage = JsonSerializer.Deserialize<RequestMSG>(ref utf8Reader2);
+            
+            C.WindowSize = receivedRequestMessage.WindowSize;
+            C.SegmentSize = receivedRequestMessage.SegmentSize;
+            C.ConID = receivedRequestMessage.ConID;
+            C.From = receivedRequestMessage.From;
+            
+            if (ErrorHandler.VerifyRequest(receivedRequestMessage, C) != ErrorType.NOERROR)
+            {
+                Console.WriteLine("Error: Wrong message type withing the request phase");
+                return ErrorType.BADREQUEST;
+            }
 
+            try
+            {
+                rep.ConID = SessionID;
+                rep.From = Server;
+                req.To = Client;
+                req.SegmentSize = C.SegmentSize;
+                req.WindowSize = C.WindowSize;
+                req.Type = Messages.REPLY;
+                byte[] replyBytes = JsonSerializer.SerializeToUtf8Bytes(rep);
+                socket.SendTo(replyBytes, replyBytes.Length, SocketFlags.None, remoteEP);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            
+            socket.ReceiveTimeout = 5000;
+            string filelocation = "";
+            if (OperatingSystem.IsWindows())
+            {
+                filelocation = req.FileName;
+            }
+            else if (OperatingSystem.IsMacOS())
+            {
+                filelocation = "../../../" + req.FileName;
+            }
+            else
+            {
+                return ErrorType.BADREQUEST;
+            }
+            
+            
+            
+            
+            
+            
+
+            return ErrorType.NOERROR;
         }
     }
 }
